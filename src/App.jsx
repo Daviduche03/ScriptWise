f8cb1b5cb140c9063264cd4e53dcb00d464801ed
daviduche03 <daviduche4809@gmail.com>
2023-09-08 12:34:36 +0100

    icon fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import ListItem from "@tiptap/extension-list-item";
 import TextStyle from "@tiptap/extension-text-style";
 import { EditorProvider, useCurrentEditor } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
   Bold,
   TextQuote,
@@ -39,7 +39,11 @@ function Tooltip({ text, children }) {
       onMouseLeave={() => setShowTooltip(false)}
     >
       {children}
-      {showTooltip && <div className="absolute bg-gray-600 text-white py-2 px-4 rounded-lg shadow-lg top-0 left-1/2 transform -translate-x-1/2 -mt-10">{text}</div>}
+      {showTooltip && (
+        <div className="absolute bg-gray-600 text-white py-2 px-4 rounded-lg shadow-lg top-0 left-1/2 transform -translate-x-1/2 -mt-10">
+          {text}
+        </div>
+      )}
     </div>
   );
 }
@@ -54,204 +58,256 @@ const MenuBar = () => {
   return (
     <div className="flex flex-wrap gap-2 bg-gray-100 p-3 rounded-t-xl mb-3">
       <Tooltip text="Bold">
-      <button
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        disabled={!editor.can().chain().focus().toggleBold().run()}
-        className={
-          editor.isActive("bold")
-            ? "is-active"
-            : "font-bold border border-gray-700 p-1"
-        }
-      >
-        <Bold />
-      </button>
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          disabled={!editor.can().chain().focus().toggleBold().run()}
+          className={
+            editor.isActive("bold")
+              ? "is-active"
+              : "font-bold border border-gray-700 p-1"
+          }
+        >
+          <Bold />
+        </button>
       </Tooltip>
-      <button
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        disabled={!editor.can().chain().focus().toggleItalic().run()}
-        className={
-          editor.isActive("italic")
-            ? "is-active"
-            : "font-bold border border-gray-700 p-1"
-        }
-      >
-        <Italic />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleStrike().run()}
-        disabled={!editor.can().chain().focus().toggleStrike().run()}
-        className={
-          editor.isActive("strike")
-            ? "is-active"
-            : "font-bold border border-gray-700 p-1"
-        }
-      >
-        <Strikethrough />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleCode().run()}
-        disabled={!editor.can().chain().focus().toggleCode().run()}
-        className={
-          editor.isActive("code")
-            ? "is-active"
-            : "font-bold border border-gray-700 p-1"
-        }
-      >
-        <Code />
-      </button>
 
-      <button
-        onClick={() => editor.chain().focus().clearNodes().run()}
-        className="font-bold border border-gray-700 p-1"
-      >
-        clear nodes
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setParagraph().run()}
-        className={
-          editor.isActive("paragraph")
-            ? "is-active"
-            : "font-bold border border-gray-700 p-1"
-        }
-      >
-        <Pilcrow />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
-        className={
-          editor.isActive("heading", { level: 1 })
-            ? "is-active"
-            : "font-bold border border-gray-700 p-1"
-        }
-      >
-        <Heading1 />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
-        className={
-          editor.isActive("heading", { level: 2 })
-            ? "is-active"
-            : "font-bold border border-gray-700 p-1"
-        }
-      >
-        <Heading2 />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
-        className={
-          editor.isActive("heading", { level: 3 })
-            ? "is-active"
-            : "font-bold border border-gray-700 p-1"
-        }
-      >
-        <Heading3 />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
-        className={
-          editor.isActive("heading", { level: 4 })
-            ? "is-active"
-            : "font-bold border border-gray-700 p-1"
-        }
-      >
-        <Heading4 />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
-        className={
-          editor.isActive("heading", { level: 5 })
-            ? "is-active"
-            : "font-bold border border-gray-700 p-1"
-        }
-      >
-        <Heading5 />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
-        className={
-          editor.isActive("heading", { level: 6 })
-            ? "is-active"
-            : "font-bold border border-gray-700 p-1"
-        }
-      >
-        <Heading6 />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBulletList().run()}
-        className={
-          editor.isActive("bulletList")
-            ? "is-active"
-            : "font-bold border border-gray-700 p-1"
-        }
-      >
-        <List />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleOrderedList().run()}
-        className={
-          editor.isActive("orderedList")
-            ? "is-active"
-            : "font-bold border border-gray-700 p-1"
-        }
-      >
-        <ListOrdered />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-        className={
-          editor.isActive("codeBlock")
-            ? "is-active"
-            : "font-bold border border-gray-700 p-1"
-        }
-      >
-        <Code />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleBlockquote().run()}
-        className={
-          editor.isActive("blockquote")
-            ? "is-active"
-            : "font-bold border border-gray-700 p-1"
-        }
-      >
-        <TextQuote />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setHorizontalRule().run()}
-        className="font-bold border border-gray-700 p-1"
-      >
-        <Minus />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setHardBreak().run()}
-        className="font-bold border border-gray-700 p-1"
-      >
-        <Scissors />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().undo().run()}
-        disabled={!editor.can().chain().focus().undo().run()}
-        className="font-bold border border-gray-700 p-1"
-      >
-        <Undo2 />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().redo().run()}
-        disabled={!editor.can().chain().focus().redo().run()}
-        className="font-bold border border-gray-700 p-1"
-      >
-        <Redo2 />
-      </button>
-      <button
-        onClick={() => editor.chain().focus().setColor("#958DF1").run()}
-        className={
-          editor.isActive("textStyle", { color: "#958DF1" })
-            ? "is-active"
-            : "font-bold border border-gray-700 p-1"
-        }
-      >
-        purple
-      </button>
+      <Tooltip text="Italic">
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          disabled={!editor.can().chain().focus().toggleItalic().run()}
+          className={
+            editor.isActive("italic")
+              ? "is-active"
+              : "font-bold border border-gray-700 p-1"
+          }
+        >
+          <Italic />
+        </button>
+      </Tooltip>
+      <Tooltip text="Strike-through">
+        <button
+          onClick={() => editor.chain().focus().toggleStrike().run()}
+          disabled={!editor.can().chain().focus().toggleStrike().run()}
+          className={
+            editor.isActive("strike")
+              ? "is-active"
+              : "font-bold border border-gray-700 p-1"
+          }
+        >
+          <Strikethrough />
+        </button>
+      </Tooltip>
+      <Tooltip text="code">
+        <button
+          onClick={() => editor.chain().focus().toggleCode().run()}
+          disabled={!editor.can().chain().focus().toggleCode().run()}
+          className={
+            editor.isActive("code")
+              ? "is-active"
+              : "font-bold border border-gray-700 p-1"
+          }
+        >
+          <Code />
+        </button>
+      </Tooltip>
+      <Tooltip text="clear nodes">
+        <button
+          onClick={() => editor.chain().focus().clearNodes().run()}
+          className="font-bold border border-gray-700 p-1"
+        >
+          clear nodes
+        </button>
+      </Tooltip>
+      <Tooltip text="paragraph">
+        <button
+          onClick={() => editor.chain().focus().setParagraph().run()}
+          className={
+            editor.isActive("paragraph")
+              ? "is-active"
+              : "font-bold border border-gray-700 p-1"
+          }
+        >
+          <Pilcrow />
+        </button>
+      </Tooltip>
+      <Tooltip text="Heading 1">
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 1 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 1 })
+              ? "is-active"
+              : "font-bold border border-gray-700 p-1"
+          }
+        >
+          <Heading1 />
+        </button>
+      </Tooltip>
+      <Tooltip text="Heading 2">
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 2 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 2 })
+              ? "is-active"
+              : "font-bold border border-gray-700 p-1"
+          }
+        >
+          <Heading2 />
+        </button>
+      </Tooltip>
+      <Tooltip text="Heading 3">
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 3 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 3 })
+              ? "is-active"
+              : "font-bold border border-gray-700 p-1"
+          }
+        >
+          <Heading3 />
+        </button>
+      </Tooltip>
+      <Tooltip text="Heading 4">
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 4 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 4 })
+              ? "is-active"
+              : "font-bold border border-gray-700 p-1"
+          }
+        >
+          <Heading4 />
+        </button>
+      </Tooltip>
+      <Tooltip text="Heading 5">
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 5 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 5 })
+              ? "is-active"
+              : "font-bold border border-gray-700 p-1"
+          }
+        >
+          <Heading5 />
+        </button>
+      </Tooltip>
+      <Tooltip text="Heading 6">
+        <button
+          onClick={() =>
+            editor.chain().focus().toggleHeading({ level: 6 }).run()
+          }
+          className={
+            editor.isActive("heading", { level: 6 })
+              ? "is-active"
+              : "font-bold border border-gray-700 p-1"
+          }
+        >
+          <Heading6 />
+        </button>
+      </Tooltip>
+      <Tooltip text="Bullet list">
+        <button
+          onClick={() => editor.chain().focus().toggleBulletList().run()}
+          className={
+            editor.isActive("bulletList")
+              ? "is-active"
+              : "font-bold border border-gray-700 p-1"
+          }
+        >
+          <List />
+        </button>
+      </Tooltip>
+      <Tooltip text="Ordered list">
+        <button
+          onClick={() => editor.chain().focus().toggleOrderedList().run()}
+          className={
+            editor.isActive("orderedList")
+              ? "is-active"
+              : "font-bold border border-gray-700 p-1"
+          }
+        >
+          <ListOrdered />
+        </button>
+      </Tooltip>
+      <Tooltip text="Code block">
+        <button
+          onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+          className={
+            editor.isActive("codeBlock")
+              ? "is-active"
+              : "font-bold border border-gray-700 p-1"
+          }
+        >
+          <Code />
+        </button>
+      </Tooltip>
+      <Tooltip text="Blockquote">
+        <button
+          onClick={() => editor.chain().focus().toggleBlockquote().run()}
+          className={
+            editor.isActive("blockquote")
+              ? "is-active"
+              : "font-bold border border-gray-700 p-1"
+          }
+        >
+          <TextQuote />
+        </button>
+      </Tooltip>
+      <Tooltip text="Horizontal line">
+        <button
+          onClick={() => editor.chain().focus().setHorizontalRule().run()}
+          className="font-bold border border-gray-700 p-1"
+        >
+          <Minus />
+        </button>
+      </Tooltip>
+      <Tooltip text="Break">
+        <button
+          onClick={() => editor.chain().focus().setHardBreak().run()}
+          className="font-bold border border-gray-700 p-1"
+        >
+          <Scissors />
+        </button>
+      </Tooltip>
+      <Tooltip text="Undo">
+        <button
+          onClick={() => editor.chain().focus().undo().run()}
+          disabled={!editor.can().chain().focus().undo().run()}
+          className="font-bold border border-gray-700 p-1"
+        >
+          <Undo2 />
+        </button>
+      </Tooltip>
+      <Tooltip text="Redo">
+        <button
+          onClick={() => editor.chain().focus().redo().run()}
+          disabled={!editor.can().chain().focus().redo().run()}
+          className="font-bold border border-gray-700 p-1"
+        >
+          <Redo2 />
+        </button>
+      </Tooltip>
+      <Tooltip text="Purple">
+        <button
+          onClick={() => editor.chain().focus().setColor("#958DF1").run()}
+          className={
+            editor.isActive("textStyle", { color: "#958DF1" })
+              ? "is-active"
+              : "font-bold border border-gray-700 p-1"
+          }
+        >
+          purple
+        </button>
+      </Tooltip>
     </div>
   );
 };
