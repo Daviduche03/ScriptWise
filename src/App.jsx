66178febf80079be40a110bebe0650f387a06407
Daviduche03 <daviduche4809@gmail.com>
2023-09-11 21:27:21 +0100

    updare

--- src/App.jsx
+++ src/App.jsx
@@ -350,34 +350,15 @@ const Footer = () => {
 
 
 const content = `
-<h2>
-  Hi there,
-</h2>
-<p>
-  this is a <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-</p>
-<ul>
-  <li>
-    That’s a bullet list with one …
-  </li>
-  <li>
-    … or two list items.
-  </li>
-</ul>
-<p>
-  Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-</p>
-<pre><code class="language-css">body {
-display: none;
-}</code></pre>
-<p>
-  I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-</p>
-<blockquote>
-  Wow, that’s amazing. Good work, boy! 👏
-  <br />
-  — Mom
-</blockquote>
+<div class="">
+  <span>
+      <h1>Introducing ScriptWise</h1> - <p>an AI powered WYSIWYG editor Built with <a href=""><em>TipTap</em></a> and <a href=""><em>Vercel AI SDK</em></a> </p>
+      
+      <p>ScriptWise is built to be an improved version of <a href=""><em>Novel</em></a> written in plain reactjs</p>
+      
+  </span>
+
+</div>
 `;
 
 function App() {
